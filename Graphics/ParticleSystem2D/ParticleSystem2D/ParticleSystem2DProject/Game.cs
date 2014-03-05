a572ff637c4359f954aaa2c2246bdf3b00c09d7c
David Ávila <david.avila.membrives@gmail.com>
2014-03-05 15:34:15 +0100

    Update projects to .Net 4.5

--- Graphics/ParticleSystem2D/ParticleSystem2D/ParticleSystem2DProject/Game.cs
+++ Graphics/ParticleSystem2D/ParticleSystem2D/ParticleSystem2DProject/Game.cs
@@ -15,51 +15,7 @@ namespace ParticleSystem2DProject
         {
             base.Initialize(application);
 
-            WaveServices.ScreenContextManager.To(new ScreenContext(new MyScene())/*, new MyTransition(TimeSpan.FromSeconds(2))*/);
-        }
-    }
-
-    public class MyTransition : ScreenContextTransition
-    {
-        private TimeSpan timeAcumulator;
-
-        private TimeSpan duration;
-
-        public MyTransition(TimeSpan duration)
-            :base()
-        {
-            this.duration = duration;
-        }
-
-        protected override void Initialize()
-        {
-            this.timeAcumulator = TimeSpan.Zero;
-        }
-
-        public override void Update(TimeSpan gameTime)
-        {
-            this.timeAcumulator += gameTime;
-
-            if(this.timeAcumulator > this.duration)
-            {
-                this.Finished = true;
-            }
-        }
-
-        public override void Draw(TimeSpan gameTime)
-        {
-            double total = this.duration.TotalSeconds;
-            double current = this.timeAcumulator.TotalSeconds;
-            float lerp = (float)(current / total);
-            Color c = Color.White * lerp;
-
-            Vector2 pos = Vector2.UnitY * WaveServices.Platform.ScreenWidth * lerp;
-
-            this.SpriteBatch.Begin(BlendMode.AlphaBlend, DepthMode.None);
-            this.SpriteBatch.Draw(this.FromTexture, Vector2.Zero, null, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 1);
-            this.SpriteBatch.Draw(this.ToTexture, pos, null, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
-
-            this.SpriteBatch.End();
+            WaveServices.ScreenContextManager.To(new ScreenContext(new MyScene()));
         }
     }
 }
