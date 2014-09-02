d6e87c2eaabd48809b379ed1ba38d5a6f9fd1ef1
Jorge C <jorgemagic@gmail.com>
2014-09-02 17:58:34 +0200

    Updated to Wave Engine 1.4

--- Graphics/RenderTarget/RenderTarget/RenderTargetProject/Game.cs
+++ Graphics/RenderTarget/RenderTarget/RenderTargetProject/Game.cs
@@ -1,110 +1,21 @@
-// Copyright (C) 2012-2013 Weekend Game Studio
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-
 #region Using Statements
 using System;
-using System.Collections.Generic;
 using WaveEngine.Common;
 using WaveEngine.Common.Graphics;
-using WaveEngine.Common.Math;
-using WaveEngine.Components;
 using WaveEngine.Framework;
-using WaveEngine.Framework.Graphics;
 using WaveEngine.Framework.Services;
-using WaveEngine.Materials;
-using WaveEngine.Framework.Physics3D;
-using WaveEngine.Components.Graphics3D;
-using WaveEngine.Components.Cameras;
 #endregion
 
 namespace RenderTargetProject
 {
     public class Game : WaveEngine.Framework.Game
     {
-        ScreenContextManager screenContextManager;
-
         public override void Initialize(IApplication application)
         {
             base.Initialize(application);
 
-            screenContextManager = WaveServices.ScreenContextManager;
-            ScreenContext screenContext = new ScreenContext(
-                new MyScene(), 
-                new MyScene2());
-
-            screenContextManager.To(screenContext);
-        }
-
-        public class MyScene : Scene
-        {
-            public RenderTarget SmallTarget;
-
-            protected override void CreateScene()
-            {
-                SmallTarget = WaveServices.GraphicsDevice.RenderTargets.CreateRenderTarget(256, 256);
-
-                RenderManager.BackgroundColor = Color.Red;
-                RenderManager.RenderTarget = SmallTarget;
-
-                DirectionalLight skylight = new DirectionalLight("SkyLight", new Vector3(1));
-                EntityManager.Add(skylight);
-
-                ViewCamera camera = new ViewCamera("MainCamera", new Vector3(0, 2, 4), Vector3.Zero);
-                EntityManager.Add(camera.Entity);
-                RenderManager.SetActiveCamera(camera.Entity);
-
-                Entity primitive = new Entity("Primitive")
-                    .AddComponent(new Transform3D())
-                    .AddComponent(new BoxCollider())
-                    .AddComponent(new Spinner() { AxisTotalIncreases = new Vector3(-1f, -1f, -1f) })
-                    .AddComponent(Model.CreateCube())
-                    .AddComponent(new MaterialsMap(new BasicMaterial(Color.Yellow) { LightingEnabled = true }))
-                    .AddComponent(new ModelRenderer());
-
-                EntityManager.Add(primitive);
-            }
-        }
-
-        public class MyScene2 : Scene
-        {
-            protected override void CreateScene()
-            {
-                RenderManager.BackgroundColor = Color.Black;
-
-                ViewCamera camera = new ViewCamera("MainCamera", new Vector3(0, 2, 4), Vector3.Zero);
-                EntityManager.Add(camera.Entity);
-                RenderManager.SetActiveCamera(camera.Entity);
-
-                DirectionalLight skylight = new DirectionalLight("SkyLight", new Vector3(1));
-                EntityManager.Add(skylight);
-
-                Entity primitive = new Entity("Primitive")
-                    .AddComponent(new Transform3D())
-                    .AddComponent(new BoxCollider())
-                    .AddComponent(new Spinner() { AxisTotalIncreases = new Vector3(1f, 1f, 1f) })
-                    .AddComponent(Model.CreateCube())
-                    .AddComponent(new MaterialsMap(new BasicMaterial((WaveServices.ScreenContextManager.CurrentContext[0] as MyScene).SmallTarget) { LightingEnabled = true }))
-                    .AddComponent(new ModelRenderer());
-
-                EntityManager.Add(primitive);
-            }
+            ScreenContext screenContext = new ScreenContext(new MyScene(), new MyScene2());
+            WaveServices.ScreenContextManager.To(screenContext);
         }
     }
 }
