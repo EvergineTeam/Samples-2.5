1589a210de6b0ae73f3eeeeb5d59f3759cc51ab9
Antonio Ávila Membrives <antonio.avila@gmail.com>
2014-09-23 17:34:44 +0100

    Updated Wall Demo with textures

--- Physics3D/WallDemo/WallDemo/WallDemoProject/MyScene.cs
+++ Physics3D/WallDemo/WallDemo/WallDemoProject/MyScene.cs
@@ -1,23 +1,3 @@
-// Copyright (C) 2014 Weekend Game Studio
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-
 #region Using Statements
 using System;
 using WaveEngine.Common;
@@ -28,9 +8,13 @@ using WaveEngine.Components.Graphics2D;
 using WaveEngine.Components.Graphics3D;
 using WaveEngine.Framework;
 using WaveEngine.Framework.Graphics;
-using WaveEngine.Framework.Physics3D;
 using WaveEngine.Framework.Resources;
 using WaveEngine.Framework.Services;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Cameras;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Physics3D;
+using WaveEngine.Components.Graphics3D;
 using WaveEngine.Materials;
 #endregion
 
@@ -40,24 +24,26 @@ namespace WallDemoProject
     {
         protected override void CreateScene()
         {
+            //Insert your scene definition here.
+
             FreeCamera camera = new FreeCamera("MainCamera", new Vector3(0, 10, 20), new Vector3(0, 5, 0))
             {
-                BackgroundColor = Color.CornflowerBlue,
+                BackgroundColor = Color.CornflowerBlue
             };
+
             camera.Entity.AddComponent(new FireBehavior());
-            EntityManager.Add(camera.Entity);            
+
+            EntityManager.Add(camera);
 
             Entity ground = new Entity("Ground")
-                .AddComponent(new Transform3D() { Position = new Vector3(0, -1, 0) })
-                .AddComponent(new BoxCollider())
-                .AddComponent(Model.CreatePlane(Vector3.Up, 50))
-                .AddComponent(new RigidBody3D() { IsKinematic = true })
-                .AddComponent(new MaterialsMap(new BasicMaterial(Color.Gray)))
-                .AddComponent(new ModelRenderer());
-
+            .AddComponent(new Transform3D() { Position = new Vector3(0, -1, 0), Scale = new Vector3(100, 1, 100) })
+            .AddComponent(new BoxCollider())
+            .AddComponent(Model.CreateCube())
+            .AddComponent(new RigidBody3D() { IsKinematic = true })
+            .AddComponent(new MaterialsMap(new BasicMaterial(Color.White)))
+            .AddComponent(new ModelRenderer());
             EntityManager.Add(ground);
 
-            #region CreateWall
             int width = 10;
             int height = 10;
             float blockWidth = 2f;
@@ -65,32 +51,37 @@ namespace WallDemoProject
             float blockLength = 1f;
 
             int n = 0;
+
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
                     n++;
                     var toAdd = CreateBox("box" + n, new Vector3(i * blockWidth + .5f * blockWidth * (j % 2) - width * blockWidth * .5f,
-                                                                 blockHeight * .5f + j * (blockHeight),
-                                                                 0),
-                                                    new Vector3(blockWidth, blockHeight, blockLength), 10);
-
+                                                                    blockHeight * .5f + j * (blockHeight),
+                                                                    0),
+                                                                    new Vector3(blockWidth, blockHeight, blockLength), 10);
                     EntityManager.Add(toAdd);
                 }
             }
-            #endregion
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            // This method is called after the CreateScene and Initialize methods and before the first Update.
         }
 
         private Entity CreateBox(string name, Vector3 position, Vector3 scale, float mass)
         {
             Entity primitive = new Entity(name)
                 .AddComponent(new Transform3D() { Position = position, Scale = scale })
-                .AddComponent(new MaterialsMap(new BasicMaterial(GetRandomColor())))
-                .AddComponent(Model.CreateCube())
+                .AddComponent(new MaterialsMap(new BasicMaterial("Content/brickTexture")))
+                .AddComponent(new Model("Content/brick"))
                 .AddComponent(new BoxCollider())
                 .AddComponent(new RigidBody3D() { Mass = mass })
                 .AddComponent(new ModelRenderer());
-
             return primitive;
         }
 
