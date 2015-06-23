e95aacf5d2119ce9e8467b526b4c826448f7968d
Javier Fernández Rodríguez <jfrodriguez@plainconcepts.com>
2015-06-23 16:32:59 +0200

    - Added Kinect Extension sample and game

---
+++ Extensions/Kinect/Kinect2DGameSample/Kinect2DGameSample/Kinect2DGameSampleProject/Behaviors/CountDownBehavior.cs
@@ -0,0 +1,89 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// CountDownBehavior
+//
+// Copyright © 2015 Wave Engine S.L. All rights reserved.
+// Use is subject to license terms.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using WaveEngine.Framework;
+using Kinect2DGameSampleProject.Entities;
+
+namespace Kinect2DGameSampleProject.Behaviors
+{
+    /// <summary>
+    /// CountDown Behavior
+    /// </summary>
+    public class CountDownBehavior : Behavior
+    {
+        /// <summary>
+        /// Gets or sets the duration.
+        /// </summary>
+        /// <value>
+        /// The duration.
+        /// </value>
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// Gets the current time.
+        /// </summary>
+        /// <value>
+        /// The current time.
+        /// </value>
+        public TimeSpan CurrentTime { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the parent.
+        /// </summary>
+        /// <value>
+        /// The parent.
+        /// </value>
+        public TimeCounter Parent { get; set; }
+
+        /// <summary>
+        /// Starts this instance.
+        /// </summary>
+        public void Start()
+        {
+            this.CurrentTime = this.Duration;
+        }
+
+        /// <summary>
+        /// Allows this instance to execute custom logic during its 
+        /// <c>Update</c>.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
+        /// <remarks>
+        /// This method will not be executed if the 
+        /// <see cref="T:WaveEngine.Framework.Component" />, or the 
+        /// <see cref="T:WaveEngine.Framework.Entity" />
+        ///             owning it are not 
+        /// <c>Active</c>.
+        /// </remarks>
+        protected override void Update(TimeSpan gameTime)
+        {
+            var update = false;
+
+            if (this.CurrentTime > TimeSpan.Zero)
+            {
+                this.CurrentTime -= gameTime;
+                update = true;
+            }
+
+            if (this.CurrentTime < TimeSpan.Zero)
+            {
+                this.CurrentTime = TimeSpan.Zero;
+                this.Parent.Finished = true;
+                update = true;
+            }
+
+            // Updates the time only if needs
+            if (update)
+            {
+                this.Parent.Textblock.Text = this.CurrentTime.ToString(@"mm'.'ss'.'ff");
+            }
+        }
+    }
+}
---
+++ Extensions/Kinect/Kinect2DGameSample/Kinect2DGameSample/Kinect2DGameSampleProject/Behaviors/FollowPadParticlesBehavior.cs
@@ -0,0 +1,124 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// FollowPadParticlesBehavior
+//
+// Copyright © 2015 Wave Engine S.L. All rights reserved.
+// Use is subject to license terms.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using Microsoft.Kinect;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Particles;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+using WaveEngine.Kinect;
+using WaveEngine.Framework;
+
+namespace Kinect2DGameSampleProject.Behaviors
+{
+    /// <summary>
+    /// Follow Pad Particles Behavior
+    /// </summary>
+    public class FollowPadParticlesBehavior : Behavior
+    {
+        /// <summary>
+        /// The particle system
+        /// </summary>
+        [RequiredComponent]
+        private ParticleSystem2D particleSystem;
+
+        /// <summary>
+        /// The transform
+        /// </summary>
+        [RequiredComponent]
+        private Transform2D transform;
+
+        /// <summary>
+        /// The kinect service
+        /// </summary>
+        private KinectService kinectService;
+
+        /// <summary>
+        /// Gets or sets the kinect body.
+        /// </summary>
+        /// <value>
+        /// The kinect body.
+        /// </value>
+        public Body KinectBody { get; set; }
+
+        /// <summary>
+        /// Gets or sets the joint to follow.
+        /// </summary>
+        /// <value>
+        /// The joint.
+        /// </value>
+        public JointType Joint { get; set; }
+
+        /// <summary>
+        /// The position
+        /// </summary>
+        private Vector2 position = Vector2.Zero;
+
+        /// <summary>
+        /// Performs further custom initialization for this instance.
+        /// </summary>
+        /// <remarks>
+        /// By default this method does nothing.
+        /// </remarks>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            this.kinectService = WaveServices.GetService<KinectService>();
+        }
+
+        /// <summary>
+        /// Allows this instance to execute custom logic during its 
+        /// <c>Update</c>.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
+        /// <remarks>
+        /// This method will not be executed if the 
+        /// <see cref="T:WaveEngine.Framework.Component" />, or the 
+        /// <see cref="T:WaveEngine.Framework.Entity" />
+        ///             owning it are not 
+        /// <c>Active</c>.
+        /// </remarks>
+        protected override void Update(TimeSpan gameTime)
+        {
+            if (this.KinectBody != null)
+            {
+                if (this.KinectBody.IsTracked)
+                {
+                    // Updates the pad particles to joint position
+                    var joint = this.KinectBody.Joints[this.Joint];
+                    var kinectPosition = this.kinectService.Mapper.MapCameraPointToColorSpace(joint.Position);
+                    this.position.X = kinectPosition.X;
+                    this.position.Y = kinectPosition.Y;
+                    this.transform.Position = this.position;
+
+                    // It's only tracked HandRight and HandLeft.
+                    switch (this.Joint)
+                    {
+                        case JointType.HandRight:
+                            this.particleSystem.Emit = this.KinectBody.HandRightState != HandState.Closed;
+                            break;
+                        case JointType.HandLeft:
+                            this.particleSystem.Emit = this.KinectBody.HandLeftState != HandState.Closed;
+                            break;
+                    }
+                }
+                else
+                {
+                    this.particleSystem.Emit = false;
+                }
+            }
+            else
+            {
+                this.particleSystem.Emit = false;
+            }
+        }
+    }
+}
