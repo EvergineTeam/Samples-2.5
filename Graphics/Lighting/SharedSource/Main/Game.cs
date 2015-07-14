9ccb52478997b1c1a6062a58a52b5f938488da3d
Marcos Cobeña Morián <marcoscm@me.com>
2015-07-14 17:22:35 +0200

    Updated Lighting sample to 2.0

--- Graphics/Lighting/SharedSource/Main/Game.cs
+++ Graphics/Lighting/SharedSource/Main/Game.cs
@@ -6,7 +6,7 @@ using WaveEngine.Framework;
 using WaveEngine.Framework.Services;
 #endregion
 
-namespace LightingProject
+namespace Lighting
 {
     public class Game : WaveEngine.Framework.Game
     {
@@ -14,8 +14,8 @@ namespace LightingProject
         {
             base.Initialize(application);
 
-            ScreenContext screenContext = new ScreenContext(new MyScene());
-            WaveServices.ScreenContextManager.To(screenContext);
+			ScreenContext screenContext = new ScreenContext(new MyScene());	
+			WaveServices.ScreenContextManager.To(screenContext);
         }
     }
 }
