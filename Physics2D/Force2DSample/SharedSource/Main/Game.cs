26b4fa1cd484582ef62dc40a52e4b2c81a4eda6d
Javier Fernández Rodríguez <jfrodriguez@plainconcepts.com>
2015-07-16 14:40:17 +0200

    - Added Force2DSample v2.0

--- Physics2D/Force2DSample/SharedSource/Main/Game.cs
+++ Physics2D/Force2DSample/SharedSource/Main/Game.cs
@@ -6,7 +6,7 @@ using WaveEngine.Framework;
 using WaveEngine.Framework.Services;
 #endregion
 
-namespace Force2DSampleProject
+namespace Force2DSample
 {
     public class Game : WaveEngine.Framework.Game
     {
@@ -14,8 +14,8 @@ namespace Force2DSampleProject
         {
             base.Initialize(application);
 
-            ScreenContext screenContext = new ScreenContext(new MyScene());
-            WaveServices.ScreenContextManager.To(screenContext);
+			ScreenContext screenContext = new ScreenContext(new MyScene());	
+			WaveServices.ScreenContextManager.To(screenContext);
         }
     }
 }
