8c462885e56acbfee24e6305ff99f31ad873eeea
Manuel Gomar <illanos@gmail.com>
2015-08-11 14:27:04 +0200

    Port Sound Sample to 2.0

--- Media/Sounds/SharedSource/Main/Game.cs
+++ Media/Sounds/SharedSource/Main/Game.cs
@@ -6,7 +6,7 @@ using WaveEngine.Framework;
 using WaveEngine.Framework.Services;
 #endregion
 
-namespace SoundsProject
+namespace Sounds
 {
     public class Game : WaveEngine.Framework.Game
     {
@@ -14,8 +14,8 @@ namespace SoundsProject
         {
             base.Initialize(application);
 
-            ScreenContext screenContext = new ScreenContext(new MyScene());
-            WaveServices.ScreenContextManager.To(screenContext);
+			ScreenContext screenContext = new ScreenContext(new MyScene());	
+			WaveServices.ScreenContextManager.To(screenContext);
         }
     }
 }
