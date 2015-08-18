1b5046813a57dd1f9e54ea497605e63883f27947
Javier Fernández Rodríguez <jfrodriguez@plainconcepts.com>
2015-08-18 13:47:27 +0200

    - Updated HingeJoint3D v2.0 sample

--- Physics3D/HingeJoint/Launchers/Windows/Program.cs
+++ Physics3D/HingeJoint/Launchers/Windows/Program.cs
@@ -7,7 +7,7 @@ namespace HingleJoint
 {
     static class Program
     {
-        [STAThread]
+		[STAThread]
         static void Main()
         {
             using (App game = new App())
--- Physics3D/HingeJoint/SharedSource/Main/Game.cs
+++ Physics3D/HingeJoint/SharedSource/Main/Game.cs
@@ -6,7 +6,7 @@ using WaveEngine.Framework;
 using WaveEngine.Framework.Services;
 #endregion
 
-namespace HingleJointProject
+namespace HingleJoint
 {
     public class Game : WaveEngine.Framework.Game
     {
@@ -14,8 +14,8 @@ namespace HingleJointProject
         {
             base.Initialize(application);
 
-            ScreenContext screenContext = new ScreenContext(new MyScene());
-            WaveServices.ScreenContextManager.To(screenContext);
+			ScreenContext screenContext = new ScreenContext(new MyScene());	
+			WaveServices.ScreenContextManager.To(screenContext);
         }
     }
 }
