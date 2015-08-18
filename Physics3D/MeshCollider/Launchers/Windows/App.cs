ee5c7cdb019a9c3189df24b54b90b9ba749127d0
Daniel Cáceres <dcaceres@plainconcepts.com>
2015-08-18 14:19:58 +0200

    MeshCollider sample to v2.0

--- Physics3D/MeshCollider/Launchers/Windows/App.cs
+++ Physics3D/MeshCollider/Launchers/Windows/App.cs
@@ -11,31 +11,31 @@ namespace MeshCollider
 {
     public class App : WaveEngine.Adapter.Application
     {
-        MeshColliderProject.Game game;
+        MeshCollider.Game game;
         SpriteBatch spriteBatch;
         Texture2D splashScreen;
         bool splashState = true;
         TimeSpan time;
         Vector2 position;
         Color backgroundSplashColor;
-
+		
         public App()
         {
-            this.Width = 800;
-            this.Height = 600;
-            this.FullScreen = false;
-            this.WindowTitle = "MeshCollider";
+            this.Width = 1280;
+            this.Height = 720;
+			this.FullScreen = false;
+			this.WindowTitle = "MeshCollider";
         }
 
         public override void Initialize()
         {
-            this.game = new MeshColliderProject.Game();
+            this.game = new MeshCollider.Game();
             this.game.Initialize(this);
-
-            #region WAVE SOFTWARE LICENSE AGREEMENT
+			
+			#region WAVE SOFTWARE LICENSE AGREEMENT
             this.backgroundSplashColor = new Color("#ebebeb");
             this.spriteBatch = new SpriteBatch(WaveServices.GraphicsDevice);
-
+            
             var resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
             string name = string.Empty;
 
@@ -59,19 +59,21 @@ namespace MeshCollider
             }
 
             position = new Vector2();
+            position.X = (this.Width / 2.0f) - (this.splashScreen.Width / 2.0f);
+            position.Y = (this.Height / 2.0f) - (this.splashScreen.Height / 2.0f);
             #endregion
         }
 
         public override void Update(TimeSpan elapsedTime)
         {
-            if (this.game != null && !this.game.HasExited)
+             if (this.game != null && !this.game.HasExited)
             {
                 if (WaveServices.Input.KeyboardState.F10 == ButtonState.Pressed)
                 {
                     this.FullScreen = !this.FullScreen;
                 }
 
-                if (this.splashState)
+				if (this.splashState)
                 {
                     #region WAVE SOFTWARE LICENSE AGREEMENT
                     this.time += elapsedTime;
@@ -79,9 +81,6 @@ namespace MeshCollider
                     {
                         this.splashState = false;
                     }
-
-                    position.X = (this.Width - this.splashScreen.Width) / 2.0f;
-                    position.Y = (this.Height - this.splashScreen.Height) / 2.0f;
                     #endregion
                 }
                 else
@@ -107,7 +106,7 @@ namespace MeshCollider
                     #region WAVE SOFTWARE LICENSE AGREEMENT
                     WaveServices.GraphicsDevice.RenderTargets.SetRenderTarget(null);
                     WaveServices.GraphicsDevice.Clear(ref this.backgroundSplashColor, ClearFlags.Target, 1);
-                    this.spriteBatch.DrawVM(this.splashScreen, this.position, Color.White);
+                    this.spriteBatch.Draw(this.splashScreen, this.position, Color.White);
                     this.spriteBatch.Render();
                     #endregion
                 }
@@ -141,6 +140,6 @@ namespace MeshCollider
                 game.OnDeactivated();
             }
         }
-    }
+	}
 }
 
