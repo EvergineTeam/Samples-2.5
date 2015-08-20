0a2d22d041cbf90ee2b7e09c9ab9c1921ec4899b
Victor Ferrer <victorfergar@gmail.com>
2015-08-20 16:11:18 +0200

    IsisTemple WaveContent included

--- Graphics/IsisTemple/SharedSource/Main/MyScene.cs
+++ Graphics/IsisTemple/SharedSource/Main/MyScene.cs
@@ -20,7 +20,7 @@ namespace IsisTemple
     {
         protected override void CreateScene()
         {
-            this.Load(@"Content/Scenes/MyScene.wscene");
+            this.Load(WaveContent.Scenes.MyScene);
         }
     }
 }
