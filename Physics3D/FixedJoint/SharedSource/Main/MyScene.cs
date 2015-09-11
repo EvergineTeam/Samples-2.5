bd1718aa37dca3b0030cafd6d98b162b1b4a1cfd
Jorgemagic <jorgemagic@gmail.com>
2015-09-11 18:53:06 +0200

    - Updated FixedJoint3D sample

--- Physics3D/FixedJoint/SharedSource/Main/MyScene.cs
+++ Physics3D/FixedJoint/SharedSource/Main/MyScene.cs
@@ -18,7 +18,7 @@ namespace FixedJoint
     {
         protected override void CreateScene()
         {
-            this.Load(@"Content/Scenes/MyScene.wscene");           
+            this.Load(WaveContent.Scenes.MyScene);           
         }
     }
 }
