b85a9e50a7e2d4bcda84c0aaf0b784065803bfbe
David Ávila Membrives <davila@plainconcepts.com>
2015-09-11 18:47:04 +0200

    Update Lighting example

--- Graphics3D/Lighting/SharedSource/Main/MyScene.cs
+++ Graphics3D/Lighting/SharedSource/Main/MyScene.cs
@@ -18,7 +18,7 @@ namespace Lighting
     {
         protected override void CreateScene()
         {
-            this.Load(@"Content/Scenes/MyScene.wscene");           
+            this.Load(WaveContent.Scenes.MyScene);           
         }
     }
 }
