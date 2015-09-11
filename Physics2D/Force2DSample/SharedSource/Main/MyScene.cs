82e8caae9a2e8a62f46720e05d88289b666506ea
Jorgemagic <jorgemagic@gmail.com>
2015-09-11 17:32:39 +0200

    - Updated Force2D sample

--- Physics2D/Force2DSample/SharedSource/Main/MyScene.cs
+++ Physics2D/Force2DSample/SharedSource/Main/MyScene.cs
@@ -18,7 +18,7 @@ namespace Force2DSample
     {
         protected override void CreateScene()
         {
-            this.Load(@"Content/Scenes/MyScene.wscene");           
+            this.Load(WaveContent.Scenes.MyScene);           
         }
     }
 }
