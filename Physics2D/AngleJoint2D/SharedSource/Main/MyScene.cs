bc019e0bc84fd22ef510195bb67c689fcbab92f9
Jorgemagic <jorgemagic@gmail.com>
2015-09-11 15:29:09 +0200

    - Updated AngleJoint2D Sample

--- Physics2D/AngleJoint2D/SharedSource/Main/MyScene.cs
+++ Physics2D/AngleJoint2D/SharedSource/Main/MyScene.cs
@@ -18,7 +18,7 @@ namespace AngleJoint2D
     {
         protected override void CreateScene()
         {
-            this.Load(@"Content/Scenes/MyScene.wscene");           
+            this.Load(WaveContent.Scenes.MyScene);           
         }
     }
 }
