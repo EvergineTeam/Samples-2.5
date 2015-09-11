1ea74df3c2a72eab9eb9c461fd8b2970ae002118
David Ávila Membrives <davila@plainconcepts.com>
2015-09-11 20:17:11 +0200

    Update Skybox Sample

--- Graphics3D/Skybox/SharedSource/Main/MyScene.cs
+++ Graphics3D/Skybox/SharedSource/Main/MyScene.cs
@@ -18,7 +18,7 @@ namespace Skybox
     {
         protected override void CreateScene()
         {
-            this.Load(@"Content/Scenes/MyScene.wscene");
+            this.Load(WaveContent.Scenes.MyScene);
         }
     }
 }
