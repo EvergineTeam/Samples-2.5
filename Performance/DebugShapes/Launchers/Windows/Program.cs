28b1917d455c7aff138a64d21319a1d70a1f1693
Juan María Laó Ramos <juanlao@gmail.com>
2015-11-06 10:36:01 +0100

    DebugShapes sample ported to 2.0

---
+++ Performance/DebugShapes/Launchers/Windows/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using WaveEngine.Adapter;
+
+namespace DebugShapes
+{
+    static class Program
+    {
+		[STAThread]
+        static void Main()
+        {
+            using (App game = new App())
+            {
+                game.Run();
+            }
+        }
+    }
+}
+
