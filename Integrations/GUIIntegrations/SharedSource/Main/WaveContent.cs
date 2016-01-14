4531d8ab2d7413f801e3d376f31ff32a101d30c3
Jorge Canton <jorgemagic@gmail.com>
2016-01-14 13:55:05 +0100

    - Updated scene Integration sample

--- Integrations/GUIIntegrations/SharedSource/Main/WaveContent.cs
+++ Integrations/GUIIntegrations/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 28/10/2015 17:45:41
+// File generated on 14/01/2016 13:42:58
 namespace TeapotSample
 {
     using System;
