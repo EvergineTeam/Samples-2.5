99ec0d5fd29d379d58f42e61849085a00bae9352
Jorge Canton <jorgemagic@gmail.com>
2016-01-14 10:59:16 +0100

    - Updated Diagnostics Sample

--- Performance/Diagnostics/SharedSource/Main/WaveContent.cs
+++ Performance/Diagnostics/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 11/09/2015 14:01:36
+// File generated on 14/01/2016 10:48:38
 namespace Diagnostics
 {
     using System;
