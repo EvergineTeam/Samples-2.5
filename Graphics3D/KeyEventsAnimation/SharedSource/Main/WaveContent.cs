24d44e73c9b3feae215ae1e2dcc3966133265cb9
Antonio Ávila Membrives <antonio.avila@gmail.com>
2016-01-26 21:34:10 +0000

    - Update KeyEventAnimation Sample

--- Graphics3D/KeyEventsAnimation/SharedSource/Main/WaveContent.cs
+++ Graphics3D/KeyEventsAnimation/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 18/01/2016 9:23:22
+// File generated on 1/26/2016 9:33:26 PM
 namespace KeyEventsAnimation
 {
     using System;
