f63e90b2eab6973658e98475d77bc4159dfef884
Juan Antonio Cano <jacanosalado@gmail.com>
2016-02-09 15:16:36 +0100

    xcopy to accommodate to Wave targets

--- Builds/build.bat
+++ Builds/build.bat
@@ -3,4 +3,7 @@ cls
 
 "NuGet\nuget.exe" "install" "WaveEngine.Tools" "-ExcludeVersion" "-ConfigFile" "NuGet\MyGet.NuGet.config"
 
+REM Copy WaveTools to accommodate to current Windows Wave Targets
+xcopy /E /I /Y WaveEngine.Tools\tools WaveEngine.Tools\v2.0\Tools\VisualEditor\
+
 FAKE.3.26.1\tools\FAKE.exe build.fsx %*
