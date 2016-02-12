9167b2b547b19afa4ec9f6f1bac30741794881bd
jcant0n <javiercantonferrero@gmail.com>
2016-02-12 14:43:41 +0100

    Added source feed to RestorePackages nuget

--- Builds/windows.fsx
+++ Builds/windows.fsx
@@ -24,6 +24,7 @@ let Exec command args =
 let RestorePackages solutionFile =
     RestoreMSSolutionPackages (fun p -> 
         { p with
+            Sources = "https://www.myget.org/F/waveengine-nightly/api/v2" :: p.Sources
             Retries = 5 
             OutputPath = Path.Combine(getFolder solutionFile, "packages") }) solutionFile
 
