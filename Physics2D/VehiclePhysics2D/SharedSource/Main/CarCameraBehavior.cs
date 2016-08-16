18d4de242f5436def183a901f8c24872d08795f4
Antonio Ávila Membrives <antonio.avila@gmail.com>
2016-08-16 11:39:17 +0100

    - Added Vehicle Physic 2D

---
+++ Physics2D/VehiclePhysics2D/SharedSource/Main/CarCameraBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using WaveEngine.Common.Attributes;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+
+namespace VehiclePhysics
+{
+    [DataContract]
+    public class CarCameraBehavior : Behavior
+    {
+        [RequiredComponent]
+        protected Transform2D transform;
+
+        [DataMember]
+        public float MinX { get; set; }
+
+        [DataMember]
+        public float MaxX { get; set; }
+
+        [DataMember]
+        [RenderPropertyAsSlider(0, 1, 0.1f)]
+        public float Smooth { get; set; }
+
+        [DataMember]
+        public Vector2 Offset { get; set; }
+
+        [DataMember]
+        [RenderPropertyAsEntity(new string[] { "WaveEngine.Framework.Graphics.Transform2D" })]
+        public string TargetPath { get; set; }
+
+        private Transform2D targetTrasnform;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            if (!string.IsNullOrEmpty(this.TargetPath))
+            {
+                this.targetTrasnform = this.EntityManager.Find(this.TargetPath)?.FindComponent<Transform2D>();
+            }
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            if (this.targetTrasnform == null)
+            {
+                return;
+            }
+
+            var position = this.targetTrasnform.Position + this.Offset;
+
+            if (position.X > this.MaxX)
+            {
+                position.X = this.MaxX;
+            }
+            else if (position.X < this.MinX)
+            {
+                position.X = this.MinX;
+            }
+
+            var currentPosition = this.transform.Position;
+
+            Vector2.SmoothStep(ref currentPosition, ref position, this.Smooth, out position);
+
+            this.transform.Position = position;
+        }
+    }
+}
---
+++ Physics2D/VehiclePhysics2D/SharedSource/Main/CarController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Math;
+using WaveEngine.Common.Physics2D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Physics2D;
+using WaveEngine.Framework.Services;
+
+namespace VehiclePhysics
+{
+    /// <summary>
+    /// Controller class of the car
+    /// </summary>
+    [DataContract]
+    public class CarController : Behavior
+    {
+        [RequiredComponent]
+        private Transform2D transform;
+
+        [RequiredComponent]
+        private RigidBody2D rigidBody;
+
+        [RequiredComponent]
+        protected WheelJoint2D[] wheels;
+
+        [DataMember]
+        public float MaxSpeed { get; set; }
+
+        [DataMember]
+        public float MinSpeed { get; set; }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            var keyboard = WaveServices.Input.KeyboardState;
+
+            bool enabled;
+            float speed;
+
+            if (keyboard.Up == WaveEngine.Common.Input.ButtonState.Pressed)
+            {
+                enabled = true;
+                speed = MaxSpeed;
+            }
+            else if (keyboard.Down == WaveEngine.Common.Input.ButtonState.Pressed)
+            {
+                enabled = true;
+                speed = MinSpeed;
+            }
+            else
+            {
+
+                enabled = false;
+                speed = 0;
+            }
+
+            foreach (var wheel in this.wheels)
+            {
+                wheel.EnableMotor = enabled;
+                wheel.MotorSpeed = speed;
+            }
+        }
+    }
+}
