b00057faf3fb92837011f29a8e13dccb51c7b7fd
jcant0n <javiercantonferrero@gmail.com>
2016-09-15 15:43:33 +0200

    Removed a unfinished sample.

--- Graphics3D/BokehSample/Launchers/MacOS/AppDelegate.designer.cs
+++
@@ -1,7 +0,0 @@
-namespace BokehSample
-{
-	[global::Foundation.Register ("AppDelegate")]
-	public partial class AppDelegate
-	{
-	}
-}
--- Graphics3D/BokehSample/Launchers/MacOS/MainWindowController.designer.cs
+++
@@ -1,7 +0,0 @@
-namespace BokehSample
-{
-	[global::Foundation.Register ("MainWindowController")]
-	public partial class MainWindowController
-	{
-	}
-}
