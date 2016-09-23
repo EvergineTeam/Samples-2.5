29556f056adf0d1c1d713865cd2c92f2a8a85f46
Jorge Canton <jorgemagic@gmail.com>
2016-09-23 12:33:53 +0200

    - Regenerated MacOSX solution to Xamarin_iOS sample

---
+++ Integrations/Xamarin_iOS/Launchers/MacOSProfile/Main.cs
@@ -0,0 +1,13 @@
+using AppKit;
+
+namespace Xamarin_iOS
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
