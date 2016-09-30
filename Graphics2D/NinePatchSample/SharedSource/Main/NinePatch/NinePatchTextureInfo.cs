1c85e7945821208529e62a5bc13cfde9e62334a8
Jorge Canton <jorgemagic@gmail.com>
2016-09-30 08:48:34 +0200

    - Added NinePatch sample

---
+++ Graphics2D/NinePatchSample/SharedSource/Main/NinePatch/NinePatchTextureInfo.cs
@@ -0,0 +1,25 @@
+﻿#region Using Statements
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Math;
+#endregion
+
+namespace NinePatchSample.NinePatch
+{
+    public class NinePatchTextureInfo
+    {
+        /// <summary>
+        /// The inner texture
+        /// </summary>
+        public Texture InnerTexture { get; set; }
+
+        /// <summary>
+        /// The stretchable rectangle
+        /// </summary>
+        public RectangleF StretchableRectangle { get; set; }
+
+        /// <summary>
+        /// The stretchable rectangle coordinates
+        /// </summary>
+        public RectangleF StretchableRectangleCoords { get; set; }
+    }
+}
