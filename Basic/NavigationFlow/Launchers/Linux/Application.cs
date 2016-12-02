f639d3d136f422914038061c702f95b2b7cb9107
Victor Ferrer <victorfergar@gmail.com>
2016-12-02 13:20:10 +0100

    Navigation Flow Linus and MacOS adapters included

---
+++ Basic/NavigationFlow/Launchers/Linux/Application.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using WaveEngine.Framework.Services;
+using System.IO;
+using System.Reflection;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework.Graphics;
+using NavigationFlow.Navigation;
+
+namespace NavigationFlow
+{
+    public class App : WaveEngine.Adapter.Application
+    {
+        NavigationFlow.Game game;
+        SpriteBatch spriteBatch;
+        Texture2D splashScreen;
+        bool splashState = true;
+        TimeSpan time;
+        Vector2 position;
+        Color backgroundSplashColor;
+        KeyboardState lastKeyboardState;
+
+        public App()
+        {
+            this.Width = 1280;
+            this.Height = 720;
+            this.FullScreen = false;
+            this.WindowTitle = "NavigationFlow";
+        }
+
+        public override void Initialize()
+        {
+            this.game = new NavigationFlow.Game();
+            this.game.Initialize(this);
+
+            #region DEFAULT SPLASHSCREEN
+            this.backgroundSplashColor = new Color("#ebebeb");
+            this.spriteBatch = new SpriteBatch(WaveServices.GraphicsDevice);
+
+            var resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            string name = string.Empty;
+
+            foreach (string item in resourceNames)
+            {
+                if (item.Contains("SplashScreen.wpk"))
+                {
+                    name = item;
+                    break;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new InvalidProgramException("License terms not agreed.");
+            }
+
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
+            {
+                this.splashScreen = WaveServices.Assets.Global.LoadAsset<Texture2D>(name, stream);
+            }
+            #endregion
+        }
+
+        public override void Update(TimeSpan elapsedTime)
+        {
+            if (this.game != null && !this.game.HasExited)
+            {
+                var keyboardState = WaveServices.Input.KeyboardState;
+
+                if (keyboardState.IsKeyReleased(Keys.F10) &&
+                    this.lastKeyboardState.IsKeyPressed(Keys.F10))
+                {
+                    this.FullScreen = !this.FullScreen;
+                }
+
+                if (this.splashState)
+                {
+                    #region DEFAULT SPLASHSCREEN
+                    position.X = (this.Width / 2.0f) - (this.splashScreen.Width / 2.0f);
+                    position.Y = (this.Height / 2.0f) - (this.splashScreen.Height / 2.0f);
+                    this.time += elapsedTime;
+                    if (time > TimeSpan.FromSeconds(2))
+                    {
+                        this.splashState = false;
+                    }
+                    #endregion
+                }
+                else
+                {
+                    if (keyboardState.IsKeyReleased(Keys.Escape) &&
+                       this.lastKeyboardState.IsKeyPressed(Keys.Escape))
+                    {
+                        var navService = WaveServices.GetService<NavigationService>();
+
+                        if (navService.CanNavigate(NavigateCommands.Back))
+                        {
+                            navService.Navigate(NavigateCommands.Back);
+                        }
+                    }
+                    else
+                    {
+                        this.game.UpdateFrame(elapsedTime);
+                    }
+                }
+            }
+        }
+
+        public override void Draw(TimeSpan elapsedTime)
+        {
+            if (this.game != null && !this.game.HasExited)
+            {
+                if (this.splashState)
+                {
+                    #region DEFAULT SPLASHSCREEN
+                    WaveServices.GraphicsDevice.RenderTargets.SetRenderTarget(null);
+                    WaveServices.GraphicsDevice.Clear(ref this.backgroundSplashColor, ClearFlags.Target, 1);
+                    this.spriteBatch.Draw(this.splashScreen, this.position, Color.White);
+                    this.spriteBatch.Render();
+                    #endregion
+                }
+                else
+                {
+                    this.game.DrawFrame(elapsedTime);
+                }
+            }
+        }
+    }
+}
+
---
+++ Basic/NavigationFlow/Launchers/MacOS/MainWindow.designer.cs
@@ -0,0 +1,7 @@
+namespace NavigationFlow
+{
+	[global::Foundation.Register ("MainWindow")]
+	public partial class MainWindow
+	{
+	}
+}
