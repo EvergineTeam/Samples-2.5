ef137c44491edcc0d8b8c484603a10ecb154a1dc
Juan Maria Lao Ramos <jlao@plainconcepts.com>
2017-05-10 16:37:42 +0200

    Set waveengine tools to allow compilation

--- Builds/runTests.bat
+++ Builds/runTests.bat
@@ -4,14 +4,18 @@ if [%1]==[] goto :blank
 
 set testFolder=%1
 
-NuGet\nuget.exe install WaveEngine.VisualTestManager -ExcludeVersion -ConfigFile NuGet\NuGet.config
+NuGet\nuget.exe install WaveEngine.WindowsTools -ExcludeVersion -ConfigFile NuGet\NuGet.config
 
+xcopy  WaveEngine.WindowsTools\tools  WaveEngine.WindowsTools\v2.0\Tools\VisualEditor /Y /I
+
+SET WaveEngine=%~dp0\WaveEngine.WindowsTools\
+
+
+NuGet\nuget.exe install WaveEngine.VisualTestManager -ExcludeVersion -ConfigFile NuGet\NuGet.config
 WaveEngine.VisualTestManager\tools\TestManager.exe batch --path %testFolder% --platform Windows
 
-echo ##teamcity[publishArtifacts %~dp0%testFolder%\report.txt]
-echo ##teamcity[publishArtifacts %~dp0%testFolder%\report.zip]
+echo ##teamcity[publishArtifacts report.txt]
+echo ##teamcity[publishArtifacts report.zip]
 
 :blank
 exit /b
-
-
