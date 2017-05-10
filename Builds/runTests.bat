be5a26dfbb4093cafc6403ee74c34cf9fe62fc35
Juan María Laó Ramos <juanl@JLAO-LAPTOP.plainconcepts.com>
2017-05-10 10:38:23 +0200

    Added script to run tests. Modified getting started

---
+++ Builds/runTests.bat
@@ -0,0 +1,17 @@
+REM @echo off
+
+if [%1]==[] goto :blank
+
+set testFolder=%1
+
+NuGet\nuget.exe install WaveEngine.VisualTestManager -ExcludeVersion -ConfigFile NuGet\NuGet.config
+
+WaveEngine.VisualTestManager\tools\TestManager.exe batch --path %testFolder% --platform Windows
+
+echo ##teamcity[publishArtifacts %~dp0%testFolder%\report.txt]
+echo ##teamcity[publishArtifacts %~dp0%testFolder%\report.zip]
+
+:blank
+exit /b
+
+
