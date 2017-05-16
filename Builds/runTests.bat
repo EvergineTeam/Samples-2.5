b6952a86aa2665a7de0220ce023e652a77ca9d24
Juan Maria Lao Ramos <jlao@plainconcepts.com>
2017-05-16 16:02:18 +0200

    Fix script to generate artifacts

--- Builds/runTests.bat
+++ Builds/runTests.bat
@@ -14,8 +14,11 @@ SET WaveEngine=%~dp0\WaveEngine.WindowsTools\
 NuGet\nuget.exe install WaveEngine.VisualTestManager -ExcludeVersion -ConfigFile NuGet\NuGet.config
 WaveEngine.VisualTestManager\tools\TestManager.exe batch --path %testFolder% --platform Windows
 
-echo ##teamcity[publishArtifacts report.txt]
-echo ##teamcity[publishArtifacts report.zip]
+set reportTXT=%~dp0%testFolder%\report.txt
+set reportZIP=%~dp0%testFolder%\report.zip
+
+echo ##teamcity[publishArtifacts %reportTXT%]
+echo ##teamcity[publishArtifacts %reportZIP%]
 
 :blank
 exit /b
