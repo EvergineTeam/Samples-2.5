e82dde4c3651ab273fc480bbafe38c7fe9d56ed2
Jorge Cantón <jorgemagic@gmail.com>
2017-08-03 12:58:42 +0200

    - InternetTexture ported to 2.4.0

--- Graphics2D/InternetTextureLoad/SharedSource/Main/WaveContent.cs
+++ Graphics2D/InternetTextureLoad/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 3/14/2017 3:39:44 PM
+// File generated on 03/08/2017 12:49:25
 namespace InternetTextureLoad
 {
     using System;
@@ -16,6 +16,9 @@ namespace InternetTextureLoad
     
     public sealed class WaveContent
     {
+        
+        /// <summary> Path to Content/InternetTextureLoad.wgame </summary>
+        public const string GameInfo = "Content/InternetTextureLoad.wgame";
         
         public sealed class Assets
         {
