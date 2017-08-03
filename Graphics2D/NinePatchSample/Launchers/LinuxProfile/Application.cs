93b43931e36fa9397550a92c50fe78a7e7c2f4e3
Jorge Cantón <jorgemagic@gmail.com>
2017-08-03 13:50:07 +0200

    - NinePatchSample ported to 2.4.0

--- Graphics2D/NinePatchSample/Launchers/LinuxProfile/Application.cs
+++ Graphics2D/NinePatchSample/Launchers/LinuxProfile/Application.cs
@@ -43,7 +43,7 @@ namespace NinePatchSample
 
             foreach (string item in resourceNames)
             {
-                if (item.Contains("SplashScreen.wpk"))
+                if (item.Contains("SplashScreen.png"))
                 {
                     name = item;
                     break;
@@ -57,7 +57,7 @@ namespace NinePatchSample
 
             using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
             {
-                this.splashScreen = WaveServices.Assets.Global.LoadAsset<Texture2D>(name, stream);
+                this.splashScreen = Texture2D.FromFile(WaveServices.GraphicsDevice, stream);
             }
             #endregion
         }
--- Graphics2D/NinePatchSample/Launchers/MacOSProfile/MainWindow.cs
+++ Graphics2D/NinePatchSample/Launchers/MacOSProfile/MainWindow.cs
@@ -55,7 +55,7 @@ namespace NinePatchSample
 
 			foreach (string item in resourceNames)
 			{
-				if (item.Contains("SplashScreen.wpk"))
+				if (item.Contains("SplashScreen.png"))
 				{
 					name = item;
 					break;
@@ -69,8 +69,8 @@ namespace NinePatchSample
 
 			using (Stream stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
 			{
-				this.splashScreen = WaveServices.Assets.Global.LoadAsset<Texture2D>(name, stream);			
-			}
+				this.splashScreen = Texture2D.FromFile(WaveServices.GraphicsDevice, stream);
+            }
 			#endregion
 		}
 
