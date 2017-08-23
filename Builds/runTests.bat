51e03600d555faec58b5ebdc7bd1ceac6de21c93
jacano <jacanosalado@gmail.com>
2017-08-23 11:56:02 +0200

    fix to really update nuget tools

--- Builds/runTests.bat
+++ Builds/runTests.bat
@@ -4,13 +4,12 @@ if [%1]==[] goto :blank
 
 set testFolder=%1
 
+rmdir /s /q WaveEngine.WindowsTools
 NuGet\nuget.exe install WaveEngine.WindowsTools -ExcludeVersion -ConfigFile NuGet\NuGet.config -PreRelease
-
 xcopy  WaveEngine.WindowsTools\tools  WaveEngine.WindowsTools\v2.0\Tools\VisualEditor /Y /I
-
 SET WaveEngine=%~dp0\WaveEngine.WindowsTools\
 
-
+rmdir /s /q WaveEngine.VisualTestManager
 NuGet\nuget.exe install WaveEngine.VisualTestManager -ExcludeVersion -ConfigFile NuGet\NuGet.config
 WaveEngine.VisualTestManager\tools\TestManager.exe batch --path %testFolder% --platform Windows
 
