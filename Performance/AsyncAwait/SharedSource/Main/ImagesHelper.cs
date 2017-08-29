53cf77d649151eae25a24a1b1e592d2c9cd24386
Daniel Cáceres <dcaceres@plainconcepts.com>
2017-08-29 15:15:57 +0200

    Async/Await sample.

---
+++ Performance/AsyncAwait/SharedSource/Main/ImagesHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class ImagesHelper
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        private static List<string> images = new List<string>
+        {
+            "https://k38.kn3.net/taringa/2/4/3/0/6/6/13/soyluuchoox/A1D.jpg?1843",
+            "https://s-media-cache-ak0.pinimg.com/originals/e1/00/1e/e1001e86903d5fccba2a7e83a0547bd4.jpg",
+            "https://ugc.kn3.net/i/origin/http://1.bp.blogspot.com/-kjh6bxfAaqU/T7Pjw5hs9lI/AAAAAAAAN14/7x932jj3HU0/s1600/Fondos+de+pantalla+con+bellos+rincones+de+la+naturaleza+(72).jpg",
+            "https://k31.kn3.net/taringa/0/C/1/2/0/0/KonahsFvZ/F6D.jpg",
+            "http://www.blogdelfotografo.com/wp-content/uploads/2015/09/paisaje-t%C3%ADpico.jpg",
+            "http://k31.kn3.net/taringa/2/9/6/A/7/2/KonahsFvZ/BE9.jpg",
+            "http://i.huffpost.com/gen/1701435/images/o-PAISAJES-MIAMI-facebook.jpg",
+            "https://s-media-cache-ak0.pinimg.com/originals/8e/90/66/8e906688e636f2db67d8fd3da82bd6bf.jpg",
+            "https://artenet.es/media/reviews/photos/original/40/b6/42/11425-nostalgia-del-ayer-paisaje-sobre-tablilla-entelada-oleo-acrilico-37-1489006882.jpg",
+            "http://imagenesdepaisajespreciosos.com/wp-content/uploads/2015/11/paisajes-oto%C3%B1ales-1-e1448792804299.jpg"
+        };
+
+        public static List<string> Images { get { return images; } }
+
+        public static ImageStreamResult LoadImageStream(string imageUrl)
+        {
+            return LoadImageStreamAsync(imageUrl, CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        public static async Task<ImageStreamResult> LoadImageStreamAsync(string imageUrl, CancellationToken cancellationToken)
+        {
+            var response = await httpClient.GetAsync(imageUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+            var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+            return new ImageStreamResult(response, stream);
+        }
+
+        public class ImageStreamResult : IDisposable
+        {
+            private IDisposable response;
+
+            public Stream Stream { get; private set; }
+
+            internal ImageStreamResult(IDisposable response, Stream stream)
+            {
+                this.response = response;
+                this.Stream = stream;
+            }
+
+            public void Dispose()
+            {
+                this.Stream.Dispose();
+                this.response.Dispose();
+            }
+        }
+    }
+}
---
+++ Performance/AsyncAwait/SharedSource/Main/MyScene.cs
@@ -0,0 +1,79 @@
+﻿using AsyncAwait.Scenes;
+using System;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.GameActions;
+using WaveEngine.Components.Gestures;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+using WaveEngine.Framework.Threading;
+
+namespace AsyncAwait
+{
+    public class MyScene : Scene
+    {
+        private Entity syncSceneLoadButton;
+        private Entity asyncImagesLoadButton;
+        private Entity asyncSceneLoadButton;
+
+        protected override void CreateScene()
+        {
+            this.Load(WaveContent.Scenes.MyScene);
+
+            this.syncSceneLoadButton = this.EntityManager.Find("buttons.syncSceneLoad");
+            this.asyncImagesLoadButton = this.EntityManager.Find("buttons.asyncImagesLoad");
+            this.asyncSceneLoadButton = this.EntityManager.Find("buttons.asyncSceneLoad");
+
+            this.syncSceneLoadButton.FindComponent<TouchGestures>().TouchTap += this.SyncSceneLoad;
+            this.asyncImagesLoadButton.FindComponent<TouchGestures>().TouchTap += this.AsyncImagesLoad;
+            this.asyncSceneLoadButton.FindComponent<TouchGestures>().TouchTap += this.AsyncSceneLoad;
+        }
+
+        private void SyncSceneLoad(object sender, GestureEventArgs e)
+        {
+            ScreenContext screenContext = new ScreenContext(new HeavyLoadScene());
+            WaveServices.ScreenContextManager.To(screenContext);
+        }
+
+        private async void AsyncImagesLoad(object sender, GestureEventArgs e)
+        {
+            await this.AnimateClick(this.asyncImagesLoadButton).AsTask().ConfigureWaveAwait(WaveTaskContinueOn.Foreground);
+            ScreenContext screenContext = new ScreenContext(new LoadImagesScene());
+            WaveServices.ScreenContextManager.To(screenContext);
+        }
+
+        private async void AsyncSceneLoad(object sender, GestureEventArgs e)
+        {
+            var sceneTask = WaveBackgroundTask.Run(() =>
+            {
+                var heavyScene = new HeavyLoadScene();
+                heavyScene.Initialize(WaveServices.GraphicsDevice);
+                return heavyScene;
+            });
+
+            await this.CreateLoopGameActionUntil(() => this.LoadingAnimation(this.asyncSceneLoadButton), () => sceneTask.IsCompleted).AsTask();
+
+            var scene = await sceneTask.ConfigureWaveAwait(WaveTaskContinueOn.Foreground);
+            ScreenContext screenContext = new ScreenContext(scene);
+            WaveServices.ScreenContextManager.To(screenContext);
+        }
+
+        public IGameAction LoadingAnimation(Entity entity)
+        {
+            var transform = entity.FindComponent<Transform2D>();
+            return new FloatAnimationGameAction(entity, 0f, 2 * MathHelper.Pi, TimeSpan.FromMilliseconds(800), EaseFunction.None, val =>
+            {
+                transform.LocalRotation = val;
+            });
+        }
+
+        public IGameAction AnimateClick(Entity entity)
+        {
+            var transform = entity.FindComponent<Transform2D>();
+            return this.CreateEmptyGameAction().ContinueWith(new FloatAnimationGameAction(entity, 1f, 0f, TimeSpan.FromMilliseconds(800), EaseFunction.BackInOutEase, v =>
+            {
+                transform.LocalScale = new Vector2(v);
+            }));
+        }
+    }
+}
