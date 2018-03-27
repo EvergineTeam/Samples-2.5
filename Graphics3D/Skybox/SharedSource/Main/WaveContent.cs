9f3ffec07d19dd791730f872c4abe3e30e8e7549
manhur <illanos@gmail.com>
2018-03-27 16:04:19 +0200

    fixed and updated  skybox to 2.5

--- Graphics3D/Skybox/SharedSource/Main/WaveContent.cs
+++ Graphics3D/Skybox/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 08/08/2017 13:50:15
+// File generated on 27/03/2018 16:03:33
 namespace Skybox
 {
     using System;
@@ -28,6 +28,28 @@ namespace Skybox
             
             /// <summary> Path to Content/Assets/metal.wmat </summary>
             public const string metal = "Content/Assets/metal.wmat";
+        }
+        
+        public sealed class RenderLayers
+        {
+            
+            /// <summary>Id of Opaque</summary>
+            public const int Opaque = 0;
+            
+            /// <summary>Id of Skybox</summary>
+            public const int Skybox = 1;
+            
+            /// <summary>Id of Alpha</summary>
+            public const int Alpha = 2;
+            
+            /// <summary>Id of Additive</summary>
+            public const int Additive = 3;
+            
+            /// <summary>Id of GUI</summary>
+            public const int GUI = 4;
+            
+            /// <summary>Id of Debug</summary>
+            public const int Debug = 5;
         }
         
         public sealed class Scenes
