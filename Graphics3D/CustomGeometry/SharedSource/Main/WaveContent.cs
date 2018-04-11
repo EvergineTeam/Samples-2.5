e6ed5fc88f14170103ac017277317e7417a76215
Juan Maria Lao Ramos <juanlao@gmail.com>
2018-04-11 16:38:17 +0200

    CustomGeometry updated to waveengine 2.5

--- Graphics3D/CustomGeometry/SharedSource/Main/WaveContent.cs
+++ Graphics3D/CustomGeometry/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 07/08/2017 15:02:41
+// File generated on 4/11/2018 4:18:43 PM
 namespace CustomGeometry
 {
     using System;
@@ -28,6 +28,28 @@ namespace CustomGeometry
             
             /// <summary> Path to Content/Assets/MyMaterial.wmat </summary>
             public const string MyMaterial = "Content/Assets/MyMaterial.wmat";
+        }
+        
+        public sealed class RenderLayers
+        {
+            
+            /// <summary>Id of Opaque</summary>
+            public const int Opaque = 0;
+            
+            /// <summary>Id of Skybox</summary>
+            public const int Skybox = 1;
+            
+            /// <summary>Id of Alpha</summary>
+            public const int Alpha = 2;
+            
+            /// <summary>Id of Additive</summary>
+            public const int Additive = 3;
+            
+            /// <summary>Id of GUI</summary>
+            public const int GUI = 4;
+            
+            /// <summary>Id of Debug</summary>
+            public const int Debug = 5;
         }
         
         public sealed class Scenes
