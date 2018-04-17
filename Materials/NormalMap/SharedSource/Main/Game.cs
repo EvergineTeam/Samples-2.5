de005b0d19a934f5ae76746e97e4b16ac5a61210
manhur <illanos@gmail.com>
2018-04-17 17:16:28 +0200

    updated normal map sample

--- Materials/NormalMap/SharedSource/Main/Game.cs
+++ Materials/NormalMap/SharedSource/Main/Game.cs
@@ -14,6 +14,8 @@ namespace NormalMap
         {
             base.Initialize(application);
 
+            this.Load(WaveContent.GameInfo);
+
 			ScreenContext screenContext = new ScreenContext(new MyScene());	
 			WaveServices.ScreenContextManager.To(screenContext);
         }
--- Materials/NormalMap/SharedSource/Main/WaveContent.cs
+++ Materials/NormalMap/SharedSource/Main/WaveContent.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// File generated on 8/3/2017 11:12:22 AM
+// File generated on 17/04/2018 17:15:29
 namespace NormalMap
 {
     using System;
@@ -37,6 +37,28 @@ namespace NormalMap
             
             /// <summary> Path to Content/Assets/material.wmat </summary>
             public const string material = "Content/Assets/material.wmat";
+        }
+        
+        public sealed class RenderLayers
+        {
+            
+            /// <summary>Id of Opaque</summary>
+            public const int Opaque = 0;
+            
+            /// <summary>Id of Skybox</summary>
+            public const int Skybox = 1;
+            
+            /// <summary>Id of Alpha</summary>
+            public const int Alpha = 2;
+            
+            /// <summary>Id of Additive</summary>
+            public const int Additive = 3;
+            
+            /// <summary>Id of GUI</summary>
+            public const int GUI = 4;
+            
+            /// <summary>Id of Debug</summary>
+            public const int Debug = 5;
         }
         
         public sealed class Scenes
