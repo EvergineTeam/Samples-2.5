da1dc436690394bcc447844807b18f4f349077d7
Víctor Ferrer García <victorfergar@gmail.com>
2018-06-21 15:18:27 +0200

    Basic samples ported to new project templates from Wave Engine v2.5

---
+++ Basic/GettingStarted/Launchers/MacOS/AppDelegate.designer.cs
@@ -0,0 +1,7 @@
+namespace GettingStarted
+{
+	[global::Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
---
+++ Basic/NavigationFlow/Launchers/MacOS/Main.cs
@@ -0,0 +1,13 @@
+using AppKit;
+
+namespace NavigationFlow
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
